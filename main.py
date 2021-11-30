6aee422fe018a13531f5d3573deb04fb1a17794f
Rudra Ashutosh <44134602+Rudra990@users.noreply.github.com>
2021-11-30 16:54:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.title('Our First Streamlit App')
+st.subheader('Introducing Streamlit in Automate Everything with Python')
+
+st.write('''This is my first web app using streamlit
+Enjoy it!!! 
+''')
